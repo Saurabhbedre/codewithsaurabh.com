affedf65df28cc36c204dc9d569e3a3b38aee628
Saurabhbedre <Sau8767509436@gmail.com>
2022-08-05 10:52:24 +0530

    log

--- app.js
+++ app.js
@@ -7,3 +7,25 @@ function myFunction() {
     x.className = "topnav";
   }
 }
+
+
+
+/*login*/
+
+function onSignIn(googleUser) {
+    var profile = googleUser.getBasicProfile();
+    $("#name").text(profile.getName());
+    $("#email").text(profile.getEmail());
+    $("#image").attr('src', profile.getImageUrl());
+    $(".data").css("display", "block");
+    $(".g-signin2").css("display", "none");
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+        alert("You have been signed out successfully");
+        $(".data").css("display", "none");
+        $(".g-signin2").css("display", "block");
+    });
+}
